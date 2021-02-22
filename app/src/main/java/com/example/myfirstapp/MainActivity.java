56391def8b66eb85d3ef79611d5c957bf915c14e
leondb999 <led.1999led@gmail.com>
2021-02-22 17:42:04 +0100

    Working DB Connection with Localhost and PHP

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -4,17 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
-    //Hallo der Test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView textView = (TextView) findViewById(R.id.textView);
+        Button lesenBtn = (Button) findViewById(R.id.lesenBtn);
+        lesenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ReadDatabase(textView).execute("someParams");
+            }
+        });
     }
 
 
 }
-
-
-
-
